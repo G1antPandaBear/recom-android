41bdb608b022655af44e4b29389551a2fcd2dde3
snack655 <cmjcmj1230@gmail.com>
2022-07-19 10:14:31 +0900

    Add : Basic setting

--- app/src/main/java/kr/hs/pandabear/recom/view/activity/MainActivity.kt
+++ app/src/main/java/kr/hs/pandabear/recom/view/activity/MainActivity.kt
@@ -1,16 +1,16 @@
 package kr.hs.pandabear.recom.view.activity
 
-import android.content.Intent
-import android.os.Bundle
-import android.speech.RecognitionListener
-import android.speech.RecognizerIntent
-import android.speech.SpeechRecognizer
-import android.widget.Toast
 import androidx.activity.viewModels
 import kr.hs.pandabear.recom.databinding.ActivityMainBinding
 import kr.hs.pandabear.recom.view.base.BaseActivity
 import kr.hs.pandabear.recom.viewmodel.activity.MainViewModel
 
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
+    override val viewModel: MainViewModel by viewModels()
 
+    override fun observerViewModel() {
+
+    }
+
+    override fun bindingViewEvent() {}
 }
