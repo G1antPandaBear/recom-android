1e9567bf58b171a1284332a05a621c9c1f43ff2a
snack655 <cmjcmj1230@gmail.com>
2022-07-19 10:50:52 +0900

    Add : Start Activity layout

---
+++ app/src/main/java/kr/hs/pandabear/recom/view/activity/StartActivity.kt
@@ -0,0 +1,14 @@
+package kr.hs.pandabear.recom.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.hs.pandabear.recom.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+
+
+    }
+}
